97476f652a68a1a995ce3c218ea7e0f4e34793ac
mswright5 <matthew181132@yahoo.com>
2014-11-20 12:19:15 -0600

    I don't know the title
    You went over the array algorithms

--- Chapter 7 Practice/ArrayTracing.java
+++ Chapter 7 Practice/ArrayTracing.java
@@ -15,27 +15,41 @@ public class ArrayTracing
     /**
      * Swaps the first and last indexes of an Array object 
      */
-    public void swapFirstAndLast() {
+    public void swapFirstAndLast() 
+    {
         int index1 = this.values[0];
         this.values[0] = this.values[this.values.length - 1];
         this.values[this.values.length - 1] = index1;
     }
     
     /**Shifts every element in an ArrayList over to the right by one index*/
-    public void shiftRight() {
-        
-    }
-    
-    /**
-     * Main method that does everything, if you don't know the main method 
-     * by now you shouldn't be reading this
-     */
-    public static void main(String[] args)
+    public void shiftRight() 
     {
-        
-        
+        int first = values[values.length - 1];
+        for (int i = this.values.length - 1; i > 0; i--){
+            this.values[i] = values[i-1];
+        }
+        values[0] = first;
     }
     
-    public int[] getValues(){return this.values;}
+    public void replaceEvenWithZero()
+    {
+        for (int i = 0; i < values.length - 1; i++){
+            if (values[i] % 2 == 0){values[i] = 0;}
+        }
+    }
+    
+    public String toString()
+    {
+        String str = "[ ";
+        
+        for (int i = 0; i < this.values.length; i++){
+            if (i > 0) {str += ", ";}
+            str += values[i];
+        }
+        
+        str += " ]";
+        return str;
+    }
 
 }
--- Chapter 7 Practice/ArrayTracingTest.java
+++ Chapter 7 Practice/ArrayTracingTest.java
@@ -31,10 +31,62 @@ public class ArrayTracingTest
         ArrayTracing array = new ArrayTracing(initialValues);
         
         array.swapFirstAndLast();
-        int swappedList[] = {25,4,9,16,1};
-        for (int i = 0; i< 5; i++){
-            assert(array.getValues()[i] == swappedList[i]);
+        String compare = "[ ";
+        for (int i = 0; i < initialValues.length; i++){
+            System.out.println(initialValues[i]);
+            if (i > 0) {compare += ", ";}
+            compare += initialValues[i];
         }
+        
+        compare += " ]";
+        
+        assert(array.toString().equals(compare));
+    }
+    
+    /**
+     * Shifts all numbers over by one index
+     */
+    @Test
+    public void shiftRightTest(){
+        int initialValues[] = {1,4,9,16,25};
+        ArrayTracing array = new ArrayTracing(initialValues);
+        
+        array.shiftRight();
+        
+        int[] shifted = {25,1,4,9,16};
+        String compare = "[ ";
+        for (int i = 0; i < shifted.length; i++){
+            if (i > 0) {compare += ", ";}
+            compare += shifted[i];
+        }
+        
+        compare += " ]";
+        
+        assert(array.toString().equals(compare));
+    }
+    
+    /**
+     * Replaces all even numbers with zero
+     */
+    @Test
+    public void replaceEvenWithZeroTest()
+    {
+        int initialValues[] = {1,4,9,16,25};
+        ArrayTracing array = new ArrayTracing(initialValues);
+        
+        array.replaceEvenWithZero();
+        initialValues[1] = 0;
+        initialValues[3] = 0;
+        
+        String compare = "[ ";
+        for (int i = 0; i < initialValues.length; i++){
+            if (i > 0) {compare += ", ";}
+            compare += initialValues[i];
+        }
+        
+        compare += " ]";
+        
+        assert(compare.equals(array.toString()));
     }
 
     /**
